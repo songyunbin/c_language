5adc9a31968563469b4c75242097ff8cb7322827
songyunbin <128668532+songyunbin@users.noreply.github.com>
2023-03-23 18:38:35 +0900

    Add files via upload

--- c_4/hello.c
+++ c_4/hello.c
@@ -5,10 +5,10 @@ int main(void)
     int price, c5000, c1000 , c500 , c100;
    
     printf("물건 값을 입력하시오:");
-    scanf("%d,&price");
+    scanf("%d",&price);
 
     printf("투입한 금액을 입력하시오:");
-    scanf("%d,&money");
+    scanf("%d",&money);
     change = money-price;
     
     c1000=change/1000;
@@ -16,6 +16,9 @@ int main(void)
 
     c500=change/500;
     change=change%500;
+
+    c100=change/100;
+    change=change%100;
     
     printf("\n천원권:%d장\n",c1000);
     printf("오백원 동전:%d개\n",c500);
